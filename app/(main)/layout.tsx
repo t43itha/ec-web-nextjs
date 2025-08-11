79da198ac7fe4f709ad64558a3452ee37ae251fd
t43itha <tabitha.opare@gmail.com>
2025-08-11 21:45:29 +0100

    footer duplication is removed

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,18 +1,7 @@
-import Header from '@/app/components/Header';
-import Footer from '@/app/components/Footer';
-
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Header />
-      <main className="min-h-screen">
-        {children}
-      </main>
-      <Footer />
-    </>
-  );
+  return <>{children}</>;
 }
